73130facc8c55e5ed4728860564b90082854ed67
bhuvanaSamala <bhuvanasamala7@gmail.com>
2024-11-11 13:44:02 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -5,3 +5,35 @@ const decodeTheRing = function (s, p) {
   };
   
   module.exports = decodeTheRing;
+  const decodeTheRing = function(s, p) {
+    const m = s.length;
+    const n = p.length;
+
+    const dp = Array.from({ length: m + 1 }, () => Array(n + 1).fill(false));
+    dp[0][0] = true;
+
+    for (let j = 1; j <= n; j++) {
+        if (p[j - 1] === '*') {
+            dp[0][j] = dp[0][j - 1];
+        }
+    }
+
+    for (let i = 1; i <= m; i++) {
+        for (let j = 1; j <= n; j++) {
+            if (p[j - 1] === s[i - 1] || p[j - 1] === '?') {
+                dp[i][j] = dp[i - 1][j - 1];
+            } else if (p[j - 1] === '*') {
+                dp[i][j] = dp[i][j - 1] || dp[i - 1][j];
+            }
+        }
+    }
+
+    return dp[m][n];
+};
+
+// Example usage:
+console.log(decodeTheRing("aa", "a"));      // Output: false
+console.log(decodeTheRing("aa", "*"));      // Output: true
+console.log(decodeTheRing("cb", "?a"));     // Output: false
+
+module.exports = decodeTheRing;
