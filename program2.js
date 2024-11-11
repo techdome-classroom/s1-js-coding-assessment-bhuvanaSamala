4b77f9d32a732ca0bb1f82c9cd7b12009a2defee
bhuvanaSamala <bhuvanasamala7@gmail.com>
2024-11-11 13:40:44 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -1,41 +1,6 @@
 const decodeTheRing = function (s, p) {
 
-    // write your code herefunction decodeTheRing(message, pattern) {
-    const memo = {};
-
-    function helper(i, j) {
-        if (i === pattern.length && j === message.length) {
-            return true;
-        }
-
-        if (i === pattern.length) {
-            return false;
-        }
-
-        if (memo[${i},${j}] !== undefined) {
-            return memo[${i},${j}];
-        }
-
-        let result = false;
-
-        if (pattern[i] === '?') {
-            result = helper(i + 1, j + 1);
-        } else if (pattern[i] === '*') {
-            result = helper(i + 1, j) || (j < message.length && helper(i, j + 1));
-        } else {
-            if (pattern[i] === message[j]) {
-                result = helper(i + 1, j + 1);
-            }
-        }
-
-        memo[${i},${j}] = result;
-        return result;
-    }
-
-    return helper(0, 0);
-}
-
- 
+    // write your code here
 
   };
   
